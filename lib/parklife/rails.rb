9eac3b7294a22c336483176cda015dcb75f2e8a3
Ben Pickles <spideryoung@gmail.com>
2019-04-05 14:36:12 +0100

    Initial config for building a Rails app.

---
+++ lib/parklife/rails.rb
@@ -0,0 +1,19 @@
+require 'fileutils'
+require 'parklife'
+require 'rake'
+
+# Allow use of the consuming Rails application's route helpers from within the
+# block when defining Parklife routes.
+Parklife::RouteSet.include(Rails.application.routes.url_helpers)
+
+Parklife.application.build_dir = Rails.root.join('build')
+Parklife.application.rack_app = Rails.application
+
+Parklife.application.before_build do |app|
+  rake_app = Rake.application
+  rake_app.init
+  rake_app.load_rakefile
+  rake_app['assets:precompile'].invoke
+
+  FileUtils.cp_r(Rails.root.join('public'), app.build_dir)
+end
