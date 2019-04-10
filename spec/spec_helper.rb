2e278a4656d40890879ab389943369af45a93391
Ben Pickles <spideryoung@gmail.com>
2019-04-10 08:28:32 +0100

    Documentation output when running a single spec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,4 +11,11 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    config.default_formatter = 'doc'
+  end
 end
