233cb6073039f291c6f920a0cdb0a68736f68417
Ben Pickles <spideryoung@gmail.com>
2019-04-15 18:50:27 +0100

    Only precompile Rails assets when the Rake task exists.

--- lib/parklife/rails.rb
+++ lib/parklife/rails.rb
@@ -1,6 +1,5 @@
 require 'fileutils'
 require 'parklife'
-require 'rake'
 
 # Allow use of the consuming Rails application's route helpers from within the
 # block when defining Parklife routes.
@@ -10,10 +9,11 @@ Parklife.application.build_dir = Rails.root.join('build')
 Parklife.application.rack_app = Rails.application
 
 Parklife.application.before_build do |app|
-  rake_app = Rake.application
-  rake_app.init
-  rake_app.load_rakefile
-  rake_app['assets:precompile'].invoke
+  Rails.application.load_tasks
+
+  if Rake::Task.task_defined?('assets:precompile')
+    Rake::Task['assets:precompile'].invoke
+  end
 
   FileUtils.cp_r(Rails.root.join('public/.'), app.build_dir)
 end
