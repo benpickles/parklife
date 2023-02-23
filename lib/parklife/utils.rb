3489866d243125a6bf55f0ca17e3353d795100ba
Ben Pickles <spideryoung@gmail.com>
2023-02-23 19:26:42 +0000

    Ignore path-less links

--- lib/parklife/utils.rb
+++ lib/parklife/utils.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Parklife
   module Utils
     extend self
@@ -14,6 +16,24 @@ module Parklife
         end
       else
         File.join(dir, path)
+      end
+    end
+
+    def scan_for_links(html)
+      doc = Nokogiri::HTML.parse(html)
+      doc.css('a').each do |a|
+        uri = URI.parse(a[:href])
+
+        # Don't visit a URL that belongs to a different domain - for now this is
+        # a guess that it's not an internal link but it also covers mailto/ftp
+        # links.
+        next if uri.host
+
+        # Don't visit a path-less URL - this will be the case for a #fragment
+        # for example.
+        next if uri.path.nil? || uri.path.empty?
+
+        yield uri.path
       end
     end
   end
