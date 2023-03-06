0f2735f78d513355a0d4d501b94f9d5f1e371ce3
Ben Pickles <spideryoung@gmail.com>
2023-03-06 22:26:03 +0000

    Fix the HOST header for a non-standard port (#81)

--- lib/parklife/browser.rb
+++ lib/parklife/browser.rb
@@ -7,16 +7,25 @@ module Parklife
     def initialize(app, base)
       @app = app
       @base = base
-      @env = {
-        'HTTP_HOST' => base.host,
-        'HTTPS' => base.scheme == 'https' ? 'on' : 'off',
-        script_name: base.path.chomp('/'),
-      }
       @session = Rack::Test::Session.new(app)
+      set_env
     end
 
     def get(path)
       session.get(path, nil, env)
     end
+
+    private
+      def set_env
+        host = base.host
+        default_port = base.scheme == 'https' ? 443 : 80
+        host += ":#{base.port}" unless base.port == default_port
+
+        @env = {
+          'HTTP_HOST' => host,
+          'HTTPS' => base.scheme == 'https' ? 'on' : 'off',
+          script_name: base.path.chomp('/'),
+        }
+      end
   end
 end
