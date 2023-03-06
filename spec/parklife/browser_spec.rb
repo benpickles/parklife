ba78a8efbf0b90e2f9e552a22c5bb0a8e82d2770
Ben Pickles <spideryoung@gmail.com>
2023-03-06 19:03:00 +0000

    Fix the HOST header for a non-standard port

--- spec/parklife/browser_spec.rb
+++ spec/parklife/browser_spec.rb
@@ -1,31 +1,6 @@
 require 'parklife/browser'
 
 RSpec.describe Parklife::Browser do
-  describe '.new setting up the correct #env' do
-    let(:app) { Proc.new {} }
-    let(:browser) { described_class.new(app, uri) }
-
-    context 'when the URL has no path' do
-      let(:uri) { URI.parse('http://foo.example.com') }
-
-      it do
-        expect(browser.env['HTTP_HOST']).to eql('foo.example.com')
-        expect(browser.env['HTTPS']).to eql('off')
-        expect(browser.env[:script_name]).to eql('')
-      end
-    end
-
-    context 'with an https URL with a path' do
-      let(:uri) { URI.parse('https://bar.example.com/baz') }
-
-      it do
-        expect(browser.env['HTTP_HOST']).to eql('bar.example.com')
-        expect(browser.env['HTTPS']).to eql('on')
-        expect(browser.env[:script_name]).to eql('/baz')
-      end
-    end
-  end
-
   describe '#get' do
     let(:app) {
       Proc.new { |env|
@@ -69,6 +44,15 @@ RSpec.describe Parklife::Browser do
       it 'strips it from the passed path' do
         response = browser.get('/baz')
         expect(response.body).to eql('https,foo.example.com,/baz,https://foo.example.com/bar/baz')
+      end
+    end
+
+    context 'when the base has a non-standard port' do
+      let(:base) { URI.parse('http://localhost:3000') }
+
+      it do
+        response = browser.get('/foo')
+        expect(response.body).to eql('http,localhost:3000,/foo,http://localhost:3000/foo')
       end
     end
   end
