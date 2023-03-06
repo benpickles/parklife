f4700481acbfb02d7a4f98eb98dfe0c67e49391f
Ben Pickles <spideryoung@gmail.com>
2023-03-06 21:55:13 +0000

    CLI command to create a starter Parkfile and friends

--- lib/parklife/cli.rb
+++ lib/parklife/cli.rb
@@ -3,6 +3,9 @@ require 'thor'
 
 module Parklife
   class CLI < Thor
+    include Thor::Actions
+    source_root File.expand_path('templates', __dir__)
+
     desc 'build', 'Create a production build'
     option :base, desc: 'Set config.base at build-time - overrides the Parkfile setting'
     def build
@@ -10,6 +13,16 @@ module Parklife
       application.config.base = options[:base] if options[:base]
 
       application.build
+    end
+
+    desc 'init', 'Generate a starter Parkfile and friends'
+    option :github_pages, desc: 'Generate a GitHub Actions workflow to deploy to GitHub Pages', type: :boolean
+    option :rails, desc: 'Include some Rails-specific settings', type: :boolean
+    option :sinatra, desc: 'Include some Sinatra-specific settings', type: :boolean
+    def init
+      template('Parkfile.erb', 'Parkfile')
+      template('static_build.erb', 'bin/static-build', mode: 0755)
+      copy_file('github_pages.yml', '.github/workflows/parklife.yml') if options[:github_pages]
     end
 
     desc 'routes', 'List all defined routes'
