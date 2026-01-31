cf0b3e548a0730b2eab277478870d677d66c01d8
Ben Pickles <spideryoung@gmail.com>
2026-01-31 16:36:37 +0000

    Only store a text/html response as an .html file

--- lib/parklife/build.rb
+++ lib/parklife/build.rb
@@ -1,20 +1,26 @@
 # frozen_string_literal: true
 module Parklife
   class Build
-    def self.path_for(path, nested_index:)
+    def self.path_for(path, media_type: nil, nested_index:)
+      # Remove leading/trailing slashes.
       path = path.gsub(/^\/|\/$/, '')
 
-      if File.extname(path).empty?
-        if path.empty?
-          'index.html'
-        elsif nested_index
-          File.join(path, 'index.html')
+      # The root is always expected to be an HTML page regardless of the
+      # response's content type.
+      return 'index.html' if path.empty?
+
+      text_html = media_type.nil? || media_type == 'text/html'
+
+      # Store a text/html response in an .html file.
+      if text_html && !path.end_with?('.html')
+        if nested_index
+          path << '/index.html'
         else
-          "#{path}.html"
+          path << '.html'
         end
-      else
-        path
       end
+
+      path
     end
 
     attr_reader :dir, :nested_index
@@ -25,7 +31,11 @@ module Parklife
     end
 
     def add(route, response)
-      build_path = self.class.path_for(route.path, nested_index: nested_index)
+      build_path = self.class.path_for(
+        route.path,
+        media_type: response.media_type,
+        nested_index: nested_index,
+      )
 
       file = dir.join(build_path)
       file.dirname.mkpath
--- spec/parklife/build_spec.rb
+++ spec/parklife/build_spec.rb
@@ -4,20 +4,60 @@ require 'parklife/build'
 RSpec.describe Parklife::Build do
   describe '.path_for' do
     [
-      ['/',               true,  'index.html'],
-      ['/bits/of/stuff',  true,  'bits/of/stuff/index.html'],
-      ['/bits/of/stuff/', true,  'bits/of/stuff/index.html'],
-      ['bits/of/stuff',   true,  'bits/of/stuff/index.html'],
-      ['/some/path.json', true,  'some/path.json'],
-      ['/',               false, 'index.html'],
-      ['/bits/of/stuff',  false, 'bits/of/stuff.html'],
-      ['/bits/of/stuff/', false, 'bits/of/stuff.html'],
-      ['/some/path.json', false, 'some/path.json'],
-      ['/index.json',     false, 'index.json'],
-    ].each do |(path, nested_index, expected)|
+      # Root paths are always saved as index.html.
+      ['/', false, 'text/html', 'index.html'],
+      ['/', true,  'text/html', 'index.html'],
+
+      # Root paths with a non-HTML content type are treated the same.
+      ['/', false, 'foo/bar', 'index.html'],
+      ['/', true,  'foo/bar', 'index.html'],
+
+      # When the content type is HTML paths are saved as HTML.
+      ['/foo', false, 'text/html', 'foo.html'],
+      ['/foo', true,  'text/html', 'foo/index.html'],
+
+      # If the path already includes an HTML extension they're left unchanged.
+      ['/foo.html', false, 'text/html', 'foo.html'],
+      ['/foo.html', true,  'text/html', 'foo.html'],
+
+      # text/html nested paths with nested_index=false.
+      ['/a/b/c',      false, 'text/html', 'a/b/c.html'],
+      ['/a/b/c.html', false, 'text/html', 'a/b/c.html'],
+      ['/a/b/c/',     false, 'text/html', 'a/b/c.html'],
+
+      # text/html nested paths with nested_index=true.
+      ['/a/b/c',      true, 'text/html', 'a/b/c/index.html'],
+      ['/a/b/c.html', true, 'text/html', 'a/b/c.html'],
+      ['/a/b/c/',     true, 'text/html', 'a/b/c/index.html'],
+
+      # Non-HTML nested paths.
+      ['/a.json',     false, 'foo/bar', 'a.json'],
+      ['/a/b/c',      false, 'foo/bar', 'a/b/c'],
+      ['/a/b/c.json', false, 'foo/bar', 'a/b/c.json'],
+      ['/a.json',     true,  'foo/bar', 'a.json'],
+      ['/a/b/c',      true,  'foo/bar', 'a/b/c'],
+      ['/a/b/c.json', true,  'foo/bar', 'a/b/c.json'],
+
+      # If the response's content type is HTML save the file as HTML regardless
+      # of the path's extension (this makes more sense with the next examples).
+      ['/a/b/c.json', false, 'text/html', 'a/b/c.json.html'],
+      ['/a/b/c.json', true,  'text/html', 'a/b/c.json/index.html'],
+
+      # If the path can be interpreted as having an extension (.8) but the
+      # response's content type is HTML then it's saved as HTML (the same as
+      # above but makes sense this time).
+      ['/dry-types/v1.8', false, 'text/html', 'dry-types/v1.8.html'],
+      ['/dry-types/v1.8', true,  'text/html', 'dry-types/v1.8/index.html'],
+
+      # Once again the path can be interpreted as having a .8 extension but
+      # the response's content type isn't HTML so it's not saved as HTML.
+      ['/dry-types/v1.8', false, 'foo/bar', 'dry-types/v1.8'],
+      ['/dry-types/v1.8', true,  'foo/bar', 'dry-types/v1.8'],
+    ].each do |(path, nested_index, media_type, expected)|
       settings = {
         path: path,
         nested_index: nested_index,
+        media_type: media_type,
       }.map { |k, v| "#{k}=#{v.inspect}" }.join(' ')
 
       context "with #{settings}" do
@@ -25,6 +65,7 @@ RSpec.describe Parklife::Build do
           expect(
             described_class.path_for(
               path,
+              media_type: media_type,
               nested_index: nested_index,
             )
           ).to eql(expected)
